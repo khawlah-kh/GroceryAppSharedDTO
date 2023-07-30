c3c235005dfcbd1c465023af3d92dc870715bb36
Khawlah Khalid <khawlah1920@hotmail.com>
2023-07-30 16:22:19 +0300

    Update GroceryCategoryRequestDTO

--- Sources/GroceryAppSharedDTO/GroceryCategoryRequestDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryRequestDTO.swift
@@ -11,9 +11,11 @@ import Foundation
 public struct GroceryCategoryRequestDTO: Codable{
     public let title: String
     public let color_code: String
+    public let uid: UUID
     
-    public init(title: String, color_code: String) {
+    public init(title: String, color_code: String, uid: UUID) {
         self.title = title
         self.color_code = color_code
+        self.uid = uid
     }
 }
