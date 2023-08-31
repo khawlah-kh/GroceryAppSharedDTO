fcd665e32a771c1f3804396f45c3b4229c59f34b
Khawlah Khalid <khawlah1920@hotmail.com>
2023-08-31 09:30:15 +0300

    Category with items

--- Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -12,10 +12,12 @@ public struct GroceryCategoryResponseDTO: Codable{
     public let id: UUID
     public let title: String
     public let color_code: String
-   // public var items : 
-    public init(id: UUID, title: String, color_code: String) {
+    public var items : [GroceryItemResponseDTO]? = nil
+    public init(id: UUID, title: String, color_code: String, items: [GroceryItemResponseDTO]?) {
         self.id = id
         self.title = title
         self.color_code = color_code
+        self.items = items
+
     }
 }
--- Sources/GroceryAppSharedDTO/Items/GroceryItemResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/Items/GroceryItemResponseDTO.swift
@@ -12,13 +12,11 @@ public struct GroceryItemResponseDTO: Codable{
     public let title: String
     public let quantity: Int
     public let price: Double
-    public var items : [GroceryItemResponseDTO]? = nil
     
-    public init(id: UUID?, title: String, quantity: Int, price: Double, items: [GroceryItemResponseDTO]?) {
+    public init(id: UUID?, title: String, quantity: Int, price: Double) {
         self.id = id
         self.title = title
         self.quantity = quantity
         self.price = price
-        self.items = items
     }
 }
